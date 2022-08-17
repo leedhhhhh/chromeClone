21a7f53f0154d80d28b7f461de68eab821879386
이동형 <leedhhhhh@gmail.com>
2022-08-17 11:45:28 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    const userName = loginInput.value;
+    console.log(userName);
+}
+
+function handleLinkClick(event) {
+    event.preventDefault();
+}
+
+loginForm.addEventListener("submit" , onLoginSubmit);
+link.addEventListener("click" , handleLinkClick);
