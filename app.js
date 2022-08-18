2b9ff28e718fa95a8ed932b60b8ddc69755bd712
이동형 <leedhhhhh@gmail.com>
2022-08-18 11:16:25 +0900

    markdown

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const userName = loginInput.value;
-    console.log(userName);
-}
-
-function handleLinkClick(event) {
-    event.preventDefault();
+    localStorage.setItem("userName" , userName);
+    greeting.innerText = `Hello ${userName}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit" , onLoginSubmit);
-link.addEventListener("click" , handleLinkClick);
