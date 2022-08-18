09224559eb57c32cdd7b730b8e8a655afbe14e7e
이동형 <leedhhhhh@gmail.com>
2022-08-18 20:49:14 +0900

    markdown

---
+++ js/clock.js
@@ -0,0 +1,12 @@
+const clock = document.querySelector("h2#clock");
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock(); // 호출안해주면 잠깐의 텀이 생겨 00:00:00을 출력해준다.
+setInterval(getClock, 1000);
