a8ff1c9a90fbf503949373d695ac533551fe7a62
이동형 <leedhhhhh@gmail.com>
2022-08-20 13:34:51 +0900

    markdown

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,13 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-List");
 
+function paintToDo(newTodo) {}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
